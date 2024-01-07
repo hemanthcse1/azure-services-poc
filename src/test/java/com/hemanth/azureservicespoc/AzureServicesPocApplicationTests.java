738a4c26a9fd878f0d3d9c9fe6788edc5f3fb49e
hemanthcse1 <hemanthcse1@gmail.com>
2024-01-07 22:47:51 +0530

    Initial commit

---
+++ src/test/java/com/hemanth/azureservicespoc/AzureServicesPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemanth.azureservicespoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureServicesPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
